75a1b31be1392a665754accbba5d727e0c375717
eclark33 <eliseclark13@gmail.com>
2025-11-14 10:59:49 -0700

    Added code for penalized regression and boosted trees

--- Allstate Claims Severity.R
+++ Allstate Claims Severity.R
@@ -5,17 +5,28 @@
 library(vroom)
 library(dbarts)
 library(embed)
+library(tidymodels)
+library(tidyverse)
+library(lightgbm)
+library(bonsai)
 
 # read in data 
 train_data <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/AllstateComp/train.csv")
 testData <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/AllstateComp/test.csv")
 
+# set factors in both test and validation sets
+train_data <- train_data %>%
+  mutate(across(starts_with("cat"), as.factor))
+testData <- testData %>%
+  mutate(across(starts_with("cat"), as.factor))
+
 
 # log transformation on loss
 train_data <- train_data %>%
-  mutate(log_loss = log1p(loss))
+  mutate(log_loss = log1p(loss)) %>%
+  select(-loss)
 
-########################## BART MODEL ##########################
+########################## BART MODEL ########################## 
 # BART model
 bart_mod <- parsnip::bart(mode = "regression") %>%
   set_engine("dbarts") %>%
@@ -23,13 +34,10 @@ bart_mod <- parsnip::bart(mode = "regression") %>%
 
 
 # recipe
-bart_recipe <- recipe(log_loss ~ . , data = train_data) %>%
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(log_loss)) %>%
-  step_pca(all_predictors(), threshold = 0.87) %>%
-  step_normalize(all_numeric_predictors)
-  
-prepped_bart_recipe <- prep(bart_recipe)
-bart_data <- bake(prepped_bart_recipe, new_data = train_data)
+bart_recipe <- recipe(log_loss ~ ., data = train_data) %>%
+  step_other(all_nominal_predictors(), threshold = 0.01) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(log_loss))
+
 
 # workflow  
 bart_workflow <- workflow() %>%
@@ -43,12 +51,13 @@ grid_of_tuning_params <- grid_regular(
 
 
 # split data for cv & run it 
-folds <- vfold_cv(log_data, v = 5, repeats=1)
+folds <- vfold_cv(train_data, v = 3, repeats = 1)
 
 CV_results <- bart_workflow %>%
   tune_grid(resamples = folds,
             grid = grid_of_tuning_params,
-            metrics = metric_set(mae))
+            metrics = metric_set(mae), 
+            control = control_grid(verbose = TRUE)) 
 
 
 # find best tuning parameters
@@ -61,28 +70,85 @@ final_wf <- bart_workflow %>%
   fit(data = train_data) 
 
 ## predictions
-bart_preds <- final_wf %>% predict(new_data = testData)
+bart_preds <- final_wf %>%
+  predict(new_data = testData) 
+
+bart_preds <- bart_preds %>%
+  mutate(loss = expm1(.pred))
+
+submission <- tibble(
+  id = testData$id,
+  loss = bart_preds$loss)
   
+
+vroom_write(x = submission, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/allstate1.csv", delim=",")
+
+
+
+
   
 ############### PENALIZED LINEAR REGRESSION ##################
 
 
 
+###################### BOOSTED TREES ####################       *Best Model 
+
+# model 
+boost_model <- boost_tree(tree_depth = tune(),
+                          trees = tune(),
+                          learn_rate = tune()) %>%
+              set_engine("lightgbm") %>%
+              set_mode("regression")
 
 
-  
-  
+# recipe
+boost_recipe <- recipe(log_loss ~ ., data = train_data) %>%
+  step_other(all_nominal_predictors(), threshold = 0.01) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(log_loss))
 
 
+# workflow  
+boost_workflow <- workflow() %>%
+  add_recipe(boost_recipe) %>%
+  add_model(boost_model)
+
+# grid of values to tune 
+grid_of_tuning_params <- grid_regular(
+  trees(range = c(50, 200)),
+  tree_depth(range = c(3, 10)),
+  learn_rate(range = c(0.1, 0.5)),
+  levels = 5)
 
 
+# split data for cv & run it 
+folds <- vfold_cv(train_data, v = 3, repeats = 1)
 
-############## BOOSTED TREES ############
+CV_results <- boost_workflow %>%
+  tune_grid(resamples = folds,
+            grid = grid_of_tuning_params,
+            metrics = metric_set(mae), 
+            control = control_grid(verbose = TRUE)) 
 
 
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "mae")
+
+## finalize the workflow & fit 
+final_wf <- boost_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train_data) 
+
+## predictions
+boost_preds <- final_wf %>%
+  predict(new_data = testData) %>%
+  mutate(loss = expm1(.pred))
 
 
+boost_submission <- tibble(
+  id = testData$id,
+  loss = boost_preds$loss)
 
-
+vroom_write(x = boost_submission, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/allstate2.csv", delim=",")
 
 
